3e8bafe3c4b11ad584181ebac11f182edd7673eb
eriksmith85 <erikwsmith85@gmail.com>
2025-11-15 19:07:24 -0500

    updated css

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import {Modal, Button} from 'react-bootstrap';
 import './ProductList.css'
 import CartItem from './CartItem';
 import {addItem} from './CartSlice';
@@ -9,7 +10,11 @@ function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState([]);
-    
+    const [openModal, setOpenModal] = useState(false);
+    const [modalText, setModalText] = useState("");
+
+  const showModal = () => setOpenModal(true);
+  const closeModal = () => setOpenModal(false);
       const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -259,6 +264,7 @@ function ProductList({ onHomeClick }) {
     const handleAddToCart = (item) => {        
         dispatch(addItem(item));
         setAddedToCart((prevState)=>[...prevState, item]);
+        showModal();
     };
     return (
         <div>
@@ -279,7 +285,7 @@ function ProductList({ onHomeClick }) {
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
-            {!showCart ? (
+            {!showCart ? (                
                 <div className="product-grid">
                     <div>
                         {plantsArray.map((category, index)=>(
@@ -300,7 +306,7 @@ function ProductList({ onHomeClick }) {
                                 </div>                                                            
                             </div>
                         ))}
-                    </div>
+                    </div>      
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
